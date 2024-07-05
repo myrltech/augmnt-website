1210a354083ae4c9dd059a99a7faf1f30e7e2f3c
akshay nagargoje <akshaynagargoje0716@gmail.com>
2024-07-05 23:12:20 +0530

    latest new logos

--- components/desktop/listings/ListingsMainCardTeams.js
+++ components/desktop/listings/ListingsMainCardTeams.js
@@ -1,20 +1,58 @@
 import React from "react";
 
+let array = [
+  "/imgs/desktop/listings/one.png",
+  "/imgs/desktop/listings/two.png",
+  "/imgs/desktop/listings/three.png",
+  "/imgs/desktop/listings/four.png",
+  "/imgs/desktop/listings/five.png",
+  "/imgs/desktop/listings/seven.png",
+  "/imgs/desktop/listings/ten.png",
+  "/imgs/desktop/listings/nine.png",
+  "/imgs/desktop/listings/eleven.png",
+  "/imgs/desktop/listings/twelve.png",
+  "/imgs/desktop/listings/thirtin.png"
+]
+
+
 export default function ListingsMainCardTeams({ title, count }) {
+
+
+  const showRandomImageOne = () => {
+    const randomIndex = Math.floor(Math.random() * array.length);
+
+    return array[randomIndex]
+    
+  };
+
+  const showRandomImageTwo = () => {
+    const randomIndex = Math.floor(Math.random() * array.length);
+
+    return array[randomIndex]
+    
+  };
+
+  const showRandomImageThree = () => {
+    const randomIndex = Math.floor(Math.random() * array.length);
+
+    return array[randomIndex]
+    
+  };
+
   return (
     <div className="col-5 d-flex align-items-center flex-nowrap listings-main-card__teamBlock">
       <h5 className="listings-main-card__teams-text1">{title}</h5>
       <div className="d-flex align-items-center listings-main-card__team-member-block">
         <img
-          src="/imgs/desktop/profile/dummy-teams-member-1.png"
+          src={showRandomImageOne()}
           alt="member"
         />
         <img
-          src="/imgs/desktop/listings/dummy-teams-member-2.png"
+          src={showRandomImageTwo()}
           alt="member"
         />
         <img
-          src="/imgs/desktop/listings/dummy-teams-member-2.png"
+          src={showRandomImageThree()}
           alt="member"
         />
       </div>
